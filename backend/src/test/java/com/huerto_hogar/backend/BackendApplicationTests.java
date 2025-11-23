ea974a51377ced7079a91f4c0be4c7bda290bbae
Vicente <vic.bravoc@duocuc.cl>
2025-11-23 17:35:26 -0300

    Se crea el proyecto con spring boot y dependencias necesarias como mysql de momento

---
+++ backend/src/test/java/com/huerto_hogar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huerto_hogar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
